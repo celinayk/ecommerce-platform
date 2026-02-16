6ab0b1f16a8171b181132f8d189995049534c700
YeonKyung Ryu <celina16161616@gmail.com>
2026-02-16 21:45:14 +0900

    오류 수정2

--- src/main/java/com/ecommerce/platform/domain/order/repository/OrderRepository.java
+++ src/main/java/com/ecommerce/platform/domain/order/repository/OrderRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
   @Query(value = "select o from Order o join fetch o.user where o.user.id = :userId",
